e1e52401942010c1ddc739b996673c38ab421412
ItzArima <skaypaprilia@gmail.com>
2021-10-22 15:06:15 +0200

    struttura dati creata

---
+++ main.js
@@ -0,0 +1,20 @@
+const team = [
+    {
+        name : 'user1',
+        role : 'studente1',
+        image : 'https://picsum.photos/images'       
+    },
+    {
+        name : 'user2',
+        role : 'studente2',
+        image : 'https://picsum.photos/images'       
+    },
+    {
+        name : 'user3',
+        role : 'studente3',
+        image : 'https://picsum.photos/images'       
+    }
+]
+
+console.log(team[1].name);
+console.log(team[2]['image']);
